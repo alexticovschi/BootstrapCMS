8931ee6f06d4c097b1869c5dfe924f11f51235c2
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-14 22:28:31 +0000

    connect to the database using CONSTANTS

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,17 @@
 <?php 
 
+$db['db_host'] = "localhost"; // hostname
+$db['db_user'] = "root"; 	  // username
+$db['db_pass'] = "root"; 	  // MySQL password
+$db['db_name'] = "cms"; 	  // database name
+
+foreach($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+
 // connect to cms database
-$connection = mysqli_connect('localhost', 'root', 'root', 'cms');
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if($connection) {
 	echo "We are connected";
