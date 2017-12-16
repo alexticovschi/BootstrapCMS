0e60105b3b0822b59241f49c1280406e8f731c98
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-16 21:34:59 +0000

    display data in categories page

--- admin/categories.php
+++ admin/categories.php
@@ -30,19 +30,37 @@
                         </div>
 
                         <div class="col-xs-6">
+                            <?php
+
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+
+                            ?>
+
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
                                         <th>Id</th>
                                         <th>Category Title</th>
-
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Lorem category</td>
-                                        <td>Ipsum category</td>
-                                    </tr>
+                                    
+                                    <?php 
+                                    
+                                    while($row = mysqli_fetch_assoc($select_categories)) {
+                                        $cat_id = $row['cat_id'];                               
+                                        $cat_title = $row['cat_title'];
+
+                                        echo "<tr>";
+                                            echo "<td>{$cat_id}</td>";
+                                            echo "<td>{$cat_title}</td>";
+                                        echo "</tr>";
+
+                                    }
+
+                                    ?>
+
                                 </tbody>
                             </table>
                         </div>
