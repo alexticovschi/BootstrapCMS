54771e8ee8d54a745255a6046b014ca45ff8da90
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-18 11:33:20 +0000

    create HTML table in posts.php and display content from posts table

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// INCLUDE update_categories.php FILE
 function include_update_categories_file() {
 	global $connection;
 
@@ -10,6 +11,7 @@ function include_update_categories_file() {
 }
 
 
+// FIND ALL CATEGORIES FROM categories TABLE
 function find_all_categories() {
 	global $connection;
 
@@ -29,7 +31,39 @@ function find_all_categories() {
     }	
 }
 
+// FIND ALL POSTS FROM posts TABLE AND DISPLAY THE CONTENT
+function find_all_posts() {
+    global $connection;
 
+    $query = "SELECT * FROM posts";
+    $select_posts = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_posts)) {
+        $post_id = $row['post_id']; 
+        $post_author = $row['post_author']; 
+        $post_title = $row['post_title'];                                   
+        $post_category_id = $row['post_category_id'];  
+        $post_status = $row['post_status'];  
+        $post_image = $row['post_image'];  
+        $post_tags = $row['post_tags'];  
+        $post_comment_count = $row['post_comment_count']; 
+        $post_date = $row['post_date'];
+
+        echo "<tr>";
+            echo "<td>$post_id</td>";
+            echo "<td>$post_author</td>";
+            echo "<td>$post_title</td>";
+            echo "<td>$post_category_id</td>";
+            echo "<td>$post_status</td>";
+            echo "<td>$post_image</td>";
+            echo "<td>$post_tags</td>";
+            echo "<td>$post_comment_count</td>";
+            echo "<td>$post_date</td>";                                      
+        echo "</tr>";
+    }    
+}
+
+// INSERT CATEGORIES IN categories TABLE
 function insert_categories() {
 	global $connection;
 
@@ -54,6 +88,7 @@ function insert_categories() {
 }
 
 
+// UPDATE CATEGORIES INTO categories TABLE
 function update_caterories() {
 	global $connection;
 
@@ -82,7 +117,7 @@ function update_caterories() {
 
 }
 
-
+// DELETE CATEGORIES FROM categories TABLE
 function delete_categories() {
 	global $connection;
 
