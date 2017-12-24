1a60bbe61a129d8e7193488c9a0b0f409eaffc6b
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-24 17:53:29 +0000

    display individual posts

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
                 $posts_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($posts_query)) {
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_content = $row['post_content'];
@@ -34,14 +35,16 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id;  ?>">
+                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                </a>
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
