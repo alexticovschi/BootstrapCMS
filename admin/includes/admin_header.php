747a6d93d9d3445f198adc38a7cc682d00d42166
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-27 20:13:45 +0000

    validate user admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -7,6 +7,15 @@
 <?php include("../includes/db.php"); ?>
 <?php include("functions.php"); ?>
 
+<?php 
+
+if(isset($_SESSION['user_role'])) {
+    if($_SESSION['user_role'] !== 'admin') {
+        header("Location: ../index.php");
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
