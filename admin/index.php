3071f298869b6856a03514978b425994e1968f27
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-30 22:13:41 +0000

    add chart and display dynamic data in it

--- admin/index.php
+++ admin/index.php
@@ -37,7 +37,7 @@
                                     $query = "SELECT * FROM posts";
                                     $select_all_posts = mysqli_query($connection, $query);
 
-                                    $post_count= mysqli_num_rows($select_all_posts);
+                                    $post_count = mysqli_num_rows($select_all_posts);
                                     echo "<div class='huge'>$post_count</div>";
 
                                     if($post_count > 1) {
@@ -74,7 +74,7 @@
                                     $query = "SELECT * FROM comments";
                                     $select_all_comments = mysqli_query($connection, $query);
 
-                                    $comment_count= mysqli_num_rows($select_all_comments);
+                                    $comment_count = mysqli_num_rows($select_all_comments);
                                     echo "<div class='huge'>$comment_count</div>";
 
                                     if($comment_count > 1) {
@@ -111,7 +111,7 @@
                                     $query = "SELECT * FROM users";
                                     $select_all_users = mysqli_query($connection, $query);
 
-                                    $user_count= mysqli_num_rows($select_all_users);
+                                    $user_count = mysqli_num_rows($select_all_users);
                                     echo "<div class='huge'>$user_count</div>";
 
                                     if($user_count > 1) {
@@ -148,7 +148,7 @@
                                     $query = "SELECT * FROM categories";
                                     $select_all_categories = mysqli_query($connection, $query);
 
-                                    $category_count= mysqli_num_rows($select_all_categories);
+                                    $category_count = mysqli_num_rows($select_all_categories);
                                     echo "<div class='huge'>$category_count</div>";
 
                                     if($category_count > 1) {
@@ -158,7 +158,7 @@
                                     }
 
                                     ?>
-                                    
+
                                     </div>
                                 </div>
                             </div>
@@ -173,6 +173,69 @@
                     </div>
                 </div>
                 <!-- /.row -->
+
+                <?php 
+
+                $query = "SELECT * FROM posts WHERE post_status = 'draft' ";
+                $draft_posts = mysqli_query($connection, $query);
+                $post_draft_count = mysqli_num_rows($draft_posts);
+
+                $query = "SELECT * FROM comments WHERE comment_status = 'unapproved' ";
+                $unapproved_comments = mysqli_query($connection, $query);
+                $unapproved_comment_count = mysqli_num_rows($unapproved_comments);
+
+                $query = "SELECT * FROM users WHERE user_role = 'subscriber' ";
+                $subscribers = mysqli_query($connection, $query);
+                $subscriber_count = mysqli_num_rows($subscribers);
+
+                ?>
+    
+
+
+
+                <div class="row">
+                    
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                      google.charts.load('current', {'packages':['bar']});
+                      google.charts.setOnLoadCallback(drawChart);
+
+                      function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                          ['Data', 'Count'],
+                        
+                            <?php 
+
+                            $element_text = ['Active Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
+                            $element_count = [$post_count, $post_draft_count,  $comment_count,  $unapproved_comment_count, $user_count, $subscriber_count, $category_count];
+
+                            for($i = 0; $i < 7; $i++) {
+                                echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
+                            } 
+
+                            ?>
+
+
+                          
+                        ]);
+
+                        var options = {
+                          chart: {
+                            title: '',
+                            subtitle: '',
+                          }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                      }
+                    </script>                
+                    
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+
+                </div>
+
 
 
             </div>
