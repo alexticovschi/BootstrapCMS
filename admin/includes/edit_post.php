b8e1ea38535dcdd2fa8952f25f9f58dff202d038
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-01 18:02:08 +0000

    display the category of the post on the select drop-down menu

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -91,8 +91,11 @@ if(isset($_GET['p_id'])) {
                     $cat_id = $row['cat_id']; 
                     $cat_title = $row['cat_title']; 
 
-
-                    echo "<option value='$cat_id'>{$cat_title}</option>";
+                    if($cat_id == $post_category_id) {
+                        echo "<option selected value='$cat_id'>{$cat_title}</option>";
+                    } else {
+                        echo "<option value='$cat_id'>{$cat_title}</option>";
+                    }
 
                 }
                 
