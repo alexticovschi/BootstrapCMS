b26a7d9419325fc3a7dfb07eb9386273e40f6fb6
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-07 20:23:35 +0000

    relating posts to their authors

--- author_posts.php
+++ author_posts.php
@@ -14,21 +14,20 @@
                 
                 <?php
 
-                $query = "SELECT * FROM posts WHERE post_status = 'published' ";
-                $posts_query = mysqli_query($connection, $query);
-
-                if (mysqli_num_rows($posts_query) == 0) {
-                    echo "<h3 style='padding: 100px; text-align: center; border: 2px solid grey; '> No Posts</h3>";
+                if(isset($_GET['p_id'])) {
+                    $post_id = $_GET['p_id'];
+                    $post_author = $_GET['author'];
                 }
 
-                while($row = mysqli_fetch_assoc($posts_query)) {
-                    $post_id = $row['post_id'];
+                $query = "SELECT * FROM posts WHERE post_author = '{$post_author}' ";
+                $post_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($post_query)) {
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
-                    $post_content = substr($row['post_content'], 0, 105) . '...';
+                    $post_content = $row['post_content'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
-                    $post_status = $row['post_status'];
 
                 ?>    
 
@@ -43,21 +42,15 @@
                     <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+                    by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <a href="post.php?p_id=<?php echo $post_id; ?>">
-                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
-                </a>
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-    
-
-
 
                 <?php } ?> <!-- close the loop -->
 
@@ -74,4 +67,3 @@
 
 <!-- Footer -->
 <?php include("includes/footer.php"); ?>
-
